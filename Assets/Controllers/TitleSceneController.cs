6574f069fe371c305d4f4818264672df4e15e1c5
Soichiro Kato <azukibeans.5kilo@gmail.com>
2021-05-16 19:53:33 +0900

    added CHANGELOG added version text in title scene

--- Assets/Controllers/TitleSceneController.cs
+++ Assets/Controllers/TitleSceneController.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 namespace Controllers
 {
     public class TitleSceneController : MonoBehaviour
     {
+        private GameObject VersionText;
+
+        void Start()
+        {
+            VersionText = GameObject.Find("VersionText");
+            VersionText.GetComponent<Text>().text = $"ver. {Application.version}";
+        }
+
         public void PlayGame()
         {
             SceneManager.LoadScene("GameScene");
